8d579ed8a33a8ad749286030f432c023bf5a0276
Nihal Lodhi <58701929+nihallodhi@users.noreply.github.com>
2022-01-02 10:46:49 +0530

    Add files via upload

---
+++ nihal_lyrics/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace nihal_lyrics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1428, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 22);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1428, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(0, 50);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(1428, 532);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label1.Location = new System.Drawing.Point(1245, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Not your song? Click here...";
+            this.label1.Click += new System.EventHandler(this.ChangeSong);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1428, 582);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Lyrics Finder - Nihal LODHI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ nihal_lyrics/Form1.cs
@@ -0,0 +1,252 @@
+﻿//using HtmlAgilityPack;
+using mshtml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.Xml;
+
+namespace nihal_lyrics
+{
+    public partial class Form1 : Form
+    {
+        String song;
+        String url;
+        int index = 0;
+        String lyrics = String.Empty;
+        String lyrics1 = String.Empty;
+        String[] song1;
+
+        WebRequest myWebRequest;
+        WebResponse myWebResponse;
+        Stream streamResponse;
+        StreamReader sreader;
+
+        String Rstring = String.Empty;
+        String Rstring1 = String.Empty;
+        String Links = String.Empty;
+
+
+        char s = '\n';
+
+
+        String[] links1;
+        String lyrics2;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            url = String.Empty;
+            song = String.Empty;
+            song = textBox1.Text; //need to enhance
+            song1 = song.Split(' ');
+            
+            for(int i=0;i<song1.Length;i++)
+            {
+                if(song.Length==1)
+                {
+                    url = "http://search.azlyrics.com/search.php?q=" + song;
+                }
+                else if(i+1==song1.Length)
+                {
+                    url = "http://search.azlyrics.com/search.php?q=" + song;
+                }
+                else
+                {
+                    url = "http://search.azlyrics.com/search.php?q=" + song +"+";
+                }
+
+            }
+           
+            
+            myWebRequest = WebRequest.Create(url);
+            myWebResponse = myWebRequest.GetResponse();
+
+            streamResponse = myWebResponse.GetResponseStream();
+
+            sreader = new StreamReader(streamResponse);
+
+                while(sreader.Read()!= 0)
+            {
+                Rstring = sreader.ReadLine();
+
+                if (Rstring.Contains("Song results:"))
+                    {
+                    break;
+                    }
+            }
+
+            Rstring1 = sreader.ReadToEnd();
+            Links = GetContent(Rstring1);
+
+            links1 = Links.Split(s);
+
+            url = String.Empty;
+
+            getinfo(links1[0]);
+
+            //myWebRequest = WebRequest.Create(links1[0]);
+            //myWebResponse = myWebRequest.GetResponse();
+
+            //streamResponse = myWebResponse.GetResponseStream();
+
+            //sreader = new StreamReader(streamResponse);
+            //Rstring = String.Empty;
+
+            //while (sreader.Read() != 0)
+            //{
+            //    Rstring = sreader.ReadLine();
+
+            //    if (Rstring.Contains("Usage of azlyrics.com content"))
+            //    {
+            //        break;
+            //    }
+            //}
+
+
+            //while (!(lyrics = sreader.ReadLine()).Contains("MxM banner"))
+            //{
+
+            //    lyrics1 += lyrics;
+            //    lyrics1 += System.Environment.NewLine;
+
+            //}
+            //lyrics2 = Regex.Replace(lyrics1,@"\<\bbr\b\>", " ");
+
+            //Rstring1 = String.Empty;
+
+            //for (int i=0;i<3;i++)
+            //{
+            //    richTextBox1.Text += links1[i];
+            //    richTextBox1.Text += System.Environment.NewLine;
+            //}
+
+            //richTextBox2.Text = lyrics2;
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    richTextBox1.Text += links1[i];
+            //    richTextBox1.Text += System.Environment.NewLine;
+            //}
+
+            streamResponse.Close();
+            sreader.Close();
+            myWebResponse.Close();
+            index++;
+        }
+
+
+        public void getinfo(String url)
+        {
+            //richTextBox1.Text = String.Empty;
+            richTextBox2.Text = String.Empty;
+            lyrics1 = String.Empty;
+            lyrics2 = String.Empty;
+
+            myWebRequest = WebRequest.Create(url);
+            myWebResponse = myWebRequest.GetResponse();
+
+            streamResponse = myWebResponse.GetResponseStream();
+
+            sreader = new StreamReader(streamResponse);
+            Rstring = String.Empty;
+
+            while (sreader.Read() != 0)
+            {
+                Rstring = sreader.ReadLine();
+
+                if (Rstring.Contains("Usage of azlyrics.com content"))
+                {
+                    break;
+                }
+            }
+
+
+            while (!(lyrics = sreader.ReadLine()).Contains("MxM banner"))
+            {
+
+                lyrics1 += lyrics;
+                lyrics1 += System.Environment.NewLine;
+
+            }
+            lyrics2 = Regex.Replace(lyrics1, @"\<\bbr\b\>", " ");
+
+            richTextBox2.Text = Regex.Replace(lyrics2, @"\<\bdiv\b\>", "");
+            Rstring1 = String.Empty;
+
+            //richTextBox2.Text = lyrics2;
+
+        }
+
+
+        private String GetContent(String Rstring)
+        {
+            String sString = "";
+            HTMLDocument d = new HTMLDocument();
+            IHTMLDocument2 doc = (IHTMLDocument2)d;
+            doc.write(Rstring);
+            String sString1 = "";
+            String sString2 = "";
+            //sString2 = 
+
+            IHTMLElementCollection L = doc.links;
+
+            foreach (IHTMLElement links in L)
+            {
+                sString += links.getAttribute("href", 0);
+                sString += "\n";
+
+                //if (sString.Contains(song))
+                //{
+                //    sString1 += sString;
+                //    sString1 += System.Environment.NewLine;
+
+                //}
+                //sString = String.Empty;
+            }
+            return sString;
+        }
+
+        private void ChangeSong(object sender, EventArgs e)
+        {
+
+            if (index == 3)
+                MessageBox.Show("Go fuck yourself! :)");
+            else
+            {                
+                getinfo(links1[index]);
+                index++;
+            }
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar==(char)13)
+            {
+
+                button1_Click(sender,e);
+            }
+        }
+    }
+}
